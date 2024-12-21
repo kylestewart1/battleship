981bf82d4488239fc1d5c11dab7d74201371f227
Kyle Stewart <kyle.b.stewart.gr@dartmouth.edu>
2024-12-21 09:54:51 -0500

    feat: computer opponent places its ships at random allowed positions

--- src/game-controller.js
+++ src/game-controller.js
@@ -2,13 +2,32 @@ import { BoardView } from "./board-view";
 import { Player } from "./player";
 
 export class GameController {
-    constructor() {
-        this.players = [new Player(1, false), new Player(2, true)];
+    constructor(againstComputer=true) {
+        this.players = [new Player(1, false), new Player(2, againstComputer)];
         this.boardViews = [new BoardView(this.players[0], false), new BoardView(this.players[1], true)];
+        if (againstComputer) {
+            this.placeShipsComputer();
+        }
         this.currentPlayer = 0;
         this.playTurn();
     }
 
+    static availableShipLengths() {
+        return [2, 3, 3, 4, 5];
+    }
+
+    placeShipsComputer() {
+        const shipLengths = GameController.availableShipLengths();
+        while (shipLengths.length > 0) {
+            const shipLength = shipLengths.pop();
+            const direction = ["up", "left", "down", "right"][Math.floor(Math.random() * 4)];
+            const row = 1 + Math.floor(Math.random() * 10);
+            const column = 1 + Math.floor(Math.random() * 10);
+            if (!this.players[1].gameboard.placeShip(row, column, shipLength, direction)) {
+                shipLengths.push(shipLength);
+            }
+        }
+    }
 
     otherPlayer() {
         return (this.currentPlayer + 1) % 2;
